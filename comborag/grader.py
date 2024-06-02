623b0118d1b6989f36d77241895578cfd71f8022
Olaf <olaf@pobox.com>
2024-06-02 23:10:37 +0000

    improved devcontainer and grader

--- comborag/grader.py
+++ comborag/grader.py
@@ -3,7 +3,7 @@ from langchain_core.output_parsers import JsonOutputParser
 
 class Grader:
     def __init__(self, llm):
-        self.prompt = PromptTemplate(
+        self.retrieval_prompt = PromptTemplate(
             template="""system
             You are a grader assessing relevance 
             of a retrieved document to a user question. If the document contains keywords related to the user question, 
@@ -15,12 +15,29 @@ class Grader:
             """,
             input_variables=["question", "document"],
         )
+        self.hallucination_prompt = PromptTemplate(
+            template="""system You are a grader assessing whether 
+            an answer is grounded in / supported by a set of facts. Give a binary score 'yes' or 'no' score to indicate 
+            whether the answer is grounded in / supported by a set of facts. Provide the binary score as a JSON with a 
+            single key 'score' and no preamble or explanation.
+            user
+            Here are the facts:
+            \n ------- \n
+            {documents} 
+            \n ------- \n
+            Here is the answer: {generation}""",
+            input_variables=["generation", "documents"],
+        )
         self.llm = llm
         self.output_parser = JsonOutputParser()
 
-    def grade(self, question):
+    def retrieval_grade(self, question):
         # docs = retriever.invoke(question)
         # doc_txt = docs[1].page_content
         doc_txt = "My computer has no drive and that's not okay."
-        retrieval_grader = self.prompt | self.llm | self.output_parser
+        retrieval_grader = self.retrieval_prompt | self.llm | self.output_parser
         return retrieval_grader.invoke({"question": question, "document": doc_txt})
+    
+    def halluciantion_grade(self, question, docs):
+        hallucination_grader = self.hallucination_prompt | self.llm | self.output_parser
+        return hallucination_grader.invoke({"question": question, "documents": docs})
